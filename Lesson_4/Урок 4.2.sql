e079f79ac3726dacf9001ac9d5aca1e838a14079
КУзнецов Виктор Владимирович <arhio@yandex.ru>
2021-09-12 18:58:17 +0300

    Урок 4

--- Lesson_4/Урок 4.2.sql
+++ Lesson_4/Урок 4.2.sql
@@ -1,3 +1,5 @@
+USE vk;
+
 CREATE TABLE `likes_types` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор сроки',
   `name` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Название типа таблицы для лайка',
@@ -17,4 +19,21 @@ CREATE TABLE `likes` (
   PRIMARY KEY (`id`),
   CONSTRAINT `likes_users_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `likes_likes_types_id` FOREIGN KEY (`likes_types_id`) REFERENCES `likes_types` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=501 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+ALTER TABLE users ADD CONSTRAINT сheck_phone CHECK (REGEXP_LIKE(phone, '^\\+7[0-9]{10}$')) ;
+
+
+CREATE TABLE `posts` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор сроки',
+  `user_id` int unsigned NOT NULL COMMENT 'Идентификатор пользователя users',
+  `media_id` int unsigned NOT NULL COMMENT 'Идентификатор типа таблицы для лайков',
+  `title` varchar(512) NOT NULL COMMENT "Название поста",
+  `body` varchar(4096) NOT NULL COMMENT "Тело поста",
+  `created_at` datetime DEFAULT current_timestamp() COMMENT 'Время создания строки',
+  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'Время обновления строки',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `posts_users_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `posts_media_id` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=501 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
